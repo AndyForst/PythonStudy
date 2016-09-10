b6edf8d88016d115d24117bbcd4b2711c902031d
AndyForst <andyxiongustb@live.com>
2016-09-11 00:38:05 +0800

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+def standard(s):
+    s = s.lower()
+    t = s[0].upper()
+    t = t + s[1:]
+    return t
+
+if __name__ == '__main__':
+    L1 = ['adam', 'LISA', 'barT']
+    L2 = list(map(standard, L1))
+    print(L2)
+    L3 = list(map(lambda x:x.capitalize(), L1))
+    print(L3)
